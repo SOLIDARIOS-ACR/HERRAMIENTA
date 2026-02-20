b994bcafbd877702f5e5b020df259e98d43f3c0f
ACREIMEX <fernanda@ubemex.edu.mx>
2026-02-20 12:28:39 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_VERSION = "v1";
+const CACHE_NAME = `herramienta-${CACHE_VERSION}`;
+
+const STATIC_ASSETS = [
+  "/HERRAMIENTA/",
+  "/HERRAMIENTA/index.html",
+  "/HERRAMIENTA/manifest.json",
+  "/HERRAMIENTA/icon-192.png",
+  "/HERRAMIENTA/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH - Network First
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+
+});
+
